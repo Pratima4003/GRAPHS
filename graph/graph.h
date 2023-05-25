559da920acd48eac21139eb8d13a0ea55ebb217f
Pratima Pore <100985414+Pratima4003@users.noreply.github.com>
2023-05-25 11:45:21 +0530

    Update graph.h

--- graph/graph.h
+++ graph/graph.h
@@ -10,3 +10,7 @@ typedef struct graph{
 void initG(graph *G, char* filename);
 void displayG(graph G);
 void BFS(graph G, int s);
+void degreeV(graph G);
+int isConnected(graph G);
+int isAdjacent(graph G, int A, int B);
+void cycle(graph G);
