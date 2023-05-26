b7e30742a2e0c069dd49cfa074e849fc3dd3c49b
Pratima Pore <100985414+Pratima4003@users.noreply.github.com>
2023-05-26 16:36:56 +0530

    Add files via upload

--- graph/main.c
+++ graph/main.c
@@ -6,6 +6,18 @@ int main(){
   graph G;
   initG(&G, "file.txt");
   displayG(G);
+  printf("\n /** BFS **/ \n");
   BFS(G, 0);
+  printf("\n /** DFS **/ \n");
+  DFS(G, 0);
+  printf("\n");
+  degreeV(G);
+  printf("isconnected = %d\n", isConnected(G));
+  printf("isAdjacent = %d\n", isAdjacent(G, 0, 0));
+  printf("\n /** CYCLES **/ \n");
+  cycle(G);
+  printf("\n /** COMPONENTS **/ \n");
+  componentsV(G);
+  printf("\n");
   return 0;
 }
