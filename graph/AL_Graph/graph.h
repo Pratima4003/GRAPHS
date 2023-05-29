a6469a0d6eeaa5af5d376f74138cbc0b367520ad
Pratima Pore <100985414+Pratima4003@users.noreply.github.com>
2023-05-29 11:11:15 +0530

    Update graph.h

--- graph/AL_Graph/graph.h
+++ graph/AL_Graph/graph.h
@@ -1,1 +1,12 @@
+//structure of graph
+typedef struct node{
+  int data;
+  struct node *next;
+}node;
 
+typedef struct graph{
+  node **list;
+  int n;
+}graph;
+
+void initG(graph *G, char *filename);
